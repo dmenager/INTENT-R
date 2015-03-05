df23d9e74946fae54b8cc69a8cad1b4eca29bc88
PrestonRobertson <p947r985@ku.edu>
2015-03-05 12:39:42 -0600

    Added selection of features and clustering-methods
    Method allows you to select features you want to cluster by specifying F, an array of strings that correspond to the desired features.
    
    Uncomment appropriate lines to try various clustering models. (We'll probably just stick with k-means.)

--- MachineLearning/TrainData.py
+++ MachineLearning/TrainData.py
@@ -34,5 +34,52 @@ for filename in os.listdir(dir_name):
 	file_counter++
 	
 #Clustering
+# X - pandas DataFrame to be clustered
+# F - array of strings corresponding to desired features
+# k - number of clusters to use in clustering model (when appropriate)
+#
+# Returns - an array of cluster labels (Y)
+def cluster(X,  F='all', k=4):
+    #
+    # Uncomment methods below to call appropriate clustering algorithm
+    #
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn.cluster.KMeans
+    clusterModel = cluster.KMeans(n_clusters = k)
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.AffinityPropagation.html#sklearn.cluster.AffinityPropagation
+    #clusterModel = cluster.AffinityPropagation()
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.MeanShift.html#sklearn.cluster.MeanShift
+    #clusterModel = cluster.MeanShift()
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html#sklearn.cluster.AgglomerativeClustering
+    #clusterModel = cluster.AgglomerativeClustering(n_clusters = k)
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.SpectralClustering.html#sklearn.cluster.SpectralClustering
+    #clusterModel = cluster.SpectralClustering(n_clusters = k)
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.FeatureAgglomeration.html#sklearn.cluster.FeatureAgglomeration	
+    #clusterModel = cluster.FeatureAgglomeration(n_clusters = k)
+    
+    # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html#sklearn.cluster.DBSCAN
+    #clusterModel = cluster.DBSCAN()
+    
+    #
+    #   Use F to determine the features to be used in clustering.
+    #
+    
+    # Everything: 'time', 'food', 'dFood', 'wood', 'dWood', 'stone', 'dStone', 'metal', 'dMetal', 'inf', 'dInf', 'wrkr', 'dWrkr', 'fmales', 'dFmales', 'cvlry', 'dCvlry', 'chmp', 'dChmp', 'hero', 'dHero', 'ships', 'dShips', 'house', 'dHouse', 'econ', 'dEcon', 'outpst', 'dOutpst', 'mltry', 'dMltry', 'fortr', 'dFortr', 'civCnt', 'dCivCnt', 'wndr', 'dWndr', 'enK', 'dEnK', 'enBldD', 'dEnBldD', 'unitsL', 'dUnitsL', 'bldL', 'dBldL', 'Label'
+    if F == 'delta':
+        X = X[['time', 'dFood', 'dWood', 'dStone', 'dMetal', 'dInf', 'dWrkr', 'dFmales', 'dCvlry', 'dChmp', 'dHero', 'dShips', 'dHouse', 'dEcon', 'dOutpst', 'dMltry', 'dFortr','dCivCnt', 'dWndr', 'dEnK', 'dEnBldD','dUnitsL', 'dBldL', 'Label']]
+    else if F == 'nonDelta':
+        X = X[['time', 'food', 'wood', 'stone',  'metal', 'inf', 'wrkr', 'fmales', 'cvlry', 'chmp', 'hero', 'ships', 'house','econ', 'outpst', 'mltry', 'fortr', 'civCnt', 'wndr', 'enK', 'enBldD', 'unitsL', 'bldL', 'Label']]
+    else if type(F) != type([]):
+        X = X
+    else:
+        X = X[F]
+        
+    return clusterModel.fit_predict(X.values)
+
 
 #use X.values to send data in a numpy array
