fb87186d4f8775597160f09f685b68b1771dda2a
PrestonRobertson <p947r985@ku.edu>
2015-04-01 10:50:49 -0500

    State is now received as a string
    Other minor changes...

--- MachineLearning/predict.py
+++ MachineLearning/predict.py
@@ -1,36 +1,28 @@
-from sklearn import svm
-import pandas as ps
-import numpy as np
-import pickle as pkl
-#from sklearn.externals import joblib
+# predict.py
 import sys
-#import os
+import pandas as ps
+from StringIO import StringIO
+from sklearn.externals import joblib as jl
 
-#clf = joblib.load('SVM.pkl')
-f = open('/home/ec2-user/INTENT-R/MachineLearning/SVM.spkl', 'r')
-clf = pkl.loads(f.read())
+fileName = 'predictSVM.pkl'
 
-filename = "/home/ec2-user/INTENT-R/clientData/" + sys.argv[1] + ".txt"
-H = ['time','food','dFood','wood','dWood','stone','dStone','metal','dMetal','inf','dInf','wrkr\
-','dWrkr','fmales','dFmales','cvlry','dCvlry','chmp','dChmp','hero','dHero','ships','dShips','\
-house','dHouse','econ','dEcon','outpst','dOutpst','mltry','dMltry','fortr','dFortr','civCnt','\
-dCivCnt','wndr','dWndr','enK','dEnK','enBldD','dEnBldD','unitsL','dUnitsL','bldL','dBldL','Lab\
-el']
-x = ps.read_csv(filename, delim_whitespace=True, names=H)
-#os.remove(filename)
+str = sys.argv[1]
 
-# Drop hand label
+headers = ps.read_csv('sample.csv',  delim_whitespace = True)
+x = ps.read_csv(StringIO(str),  names = headers.columns,  
+header = None,  delim_whitespace = True)
+
+# Remove feature(s)
 x = x.drop("Label", 1)
 
-# Combination feature(s)
+# Create feature(s)
 x["army"] = x['inf'] + x['cvlry'] + (5 * x['chmp']) + (10 * x['hero'])
 
 # Reorder alphabetically
-x = x[['army','bldL','chmp','civCnt','cvlry','dBldL','dChmp','dCivCnt','dCvlry','dEcon','dEnBl\
-dD','dEnK','dFmales','dFood','dFortr','dHero','dHouse','dInf','dMetal','dMltry','dOutpst','dSh\
-ips','dStone','dUnitsL','dWndr','dWood','dWrkr','econ','enBldD','enK','fmales','food','fortr',\
-'hero','house','inf','metal','mltry','outpst','ships','stone','time','unitsL','wndr','wood','w\
-rkr']]
+x = x.reindex_axis(sorted(x.columns),  axis = 1)
 
-pred = clf.predict(x.values)
-print("Machine learning prediction is " + pred[0])
+Labels = ["defend",  "build",  "attack"]
+clf = jl.load(fileName)    
+pred = clf.predict(x.values) 
+
+print "Machine learning prediction is " + Labels[pred[0]]
