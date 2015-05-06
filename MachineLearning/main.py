814441cfb0c86cd115a39e45f703ca5c5de4bf41
PrestonRobertson <p947r985@ku.edu>
2015-05-06 10:43:04 -0500

    Update main.py

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -1,22 +1,62 @@
-# MachineLearning.py
 import utils
 import machine_learning
+import numpy as np
 
-print "Combining data files..."
+# Useable labels
+All = ["time", "CurrentFood", "FoodUsed", "FoodSold", "FoodBought", "FoodGathered",
+"CurrentWood",  "WoodUsed", "WoodSold", "WoodBought", "WoodGathered", 
+"CurrentMetal", "MetalUsed", "MetalSold", "MetalBought", "MetalGathered", 
+"CurrentStone", "StoneUsed", "StoneSold", "StoneBought", "StoneGathered", 
+"InfantryGained", "InfantryLost", "InfantryKilled", 
+"CavalryGained", "CavalryLost", "CavalryKilled", 
+"SupportGained", "SupportLost", "SupportKilled", 
+"SiegeGained", "SiegeLost", "SiegeKilled", 
+"StructuresGained", "StructuresLost", "StructuresDestroyed", 
+"dCurrentFood", "dFoodUsed", "dFoodSold", "dFoodBought", "dFoodGathered",
+"dCurrentWood", "dWoodUsed", "dWoodSold", "dWoodBought", "dWoodGathered", 
+"dCurrentMetal", "dMetalUsed", "dMetalSold", "dMetalBought", "dMetalGathered", 
+"dCurrentStone", "dStoneUsed", "dStoneSold", "dStoneBought", "dStoneGathered", 
+"dInfantryGained", "dInfantryLost", "dInfantryKilled", 
+"dCavalryGained", "dCavalryLost", "dCavalryKilled", 
+"dSupportGained", "dSupportLost", "dSupportKilled", 
+"dSiegeGained", "dSiegeLost", "dSiegeKilled", 
+"dStructuresGained", "dStructuresLost", "dStructuresDestroyed", 
+"MilitaryMovementsOccurred",  "dMilitaryMovementsOccurred",
+"SupportMovementsOccurred", "dSupportMovementsOccurred",
+"DistanceEnemyBase", "dDistanceEnemyBase"]
+
+# Delta labels
+dAll = ["dCurrentFood", "dFoodUsed", "dFoodSold", "dFoodBought", "dFoodGathered",
+"dCurrentWood", "dWoodUsed", "dWoodSold", "dWoodBought", "dWoodGathered", 
+"dCurrentMetal", "dMetalUsed", "dMetalSold", "dMetalBought", "dMetalGathered", 
+"dCurrentStone", "dStoneUsed", "dStoneSold", "dStoneBought", "dStoneGathered", 
+"dInfantryGained", "dInfantryLost", "dInfantryKilled", 
+"dCavalryGained", "dCavalryLost", "dCavalryKilled", 
+"dSupportGained", "dSupportLost", "dSupportKilled", 
+"dSiegeGained", "dSiegeLost", "dSiegeKilled", 
+"dStructuresGained", "dStructuresLost", "dStructuresDestroyed", 
+"MilitaryMovementsOccurred",  "SupportMovementsOccurred", "dDistanceEnemyBase"]
+
+print "Gathering and initializing data..."
 utils.combine()
-print "Done."
-
 X = utils.getData()
-
-print "Calculating post-game features..."
-X = utils.calcFeatures(X)
+X[All] = machine_learning.norm(X[All])
 print "Done."
 
-Y = X.place.values
+print "Hierarchial clustering..."
+hierarchy = machine_learning.recursiveCluster(X[dAll], size = 500)
+Y = machine_learning.flatten(hierarchy, min = 40)
+X = X[Y >= 0] # Eliminating outliers
+Y = Y[Y >= 0]
+y_values = np.unique(Y)
+for i in range(0, len(y_values)):
+    Y[Y==y_values[i]] = i
+print "Done."
 
-# Restrict X to selected features
-F = ["SupportGained", "SupportLost",  "SupportKilled",  
-"StructuresGained", "StructuresLost",  "StructuresDestroyed", "SiegeGained", "SiegeLost", "SiegeKilled",  "time"]
+print "Visualizing..."
+machine_learning.pca(X[dAll], Y)
+machine_learning.hist(X[["time"]], Y)
+print "Done."
 
 print "Shifting and randomizing..."
 shift = 10
@@ -24,17 +64,12 @@ X,  Y = utils.shiftLabels (X,  Y,  shift)
 X,  Y = utils.randomize (X,  Y)
 print 'Done.'
 
-fractionTest = 0.3
-trainX,  testX = utils.divide(X,  fractionTest)
-trainY,  testY = utils.divide(Y,  fractionTest)
-
-print "Calculating accuracy..."
-clf_test_data = machine_learning.train(trainX,  trainY,  F)
-timeBound = 60
-acc = machine_learning.accuracy(clf_test_data,  testX,  testY, F,  timeBound)
-print "Accuracy for first " + str(timeBound) + " mintues: ",  acc
-
-print "Training classifier..."
-clf_all_data = machine_learning.train(X,  Y,  F)
-utils.save(clf_test_data,  'placeSVM.spkl')
+print "Choosing best parameters for classifier..."
+clf, clf_acc, test_acc = machine_learning.best_classifier(X[All], Y)
 print "Done."
+print "Classifier accuracy: ", clf_acc
+print "Test data accuracy: ", test_acc
+print "Classifier model: ", clf
+
+utils.save(clf,  'SVM.spkl')
+print "Classifier saved."
